18a61eac0d688b4d6d64e9be9cd50d001edee754
Wendy Pamella <wendypamella99@gmail.com>
2022-03-29 23:45:44 +0300

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+{
+			if (haystack[i] != needle[i])
+				break;
+
+		}
+		if (i == s)
+			return (haystack);
+		else
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}
